27e7ca505f0faf36f85afcef3b5155788bc522d1
Nerly Ton <nerlyton@gmail.com>
2020-08-03 18:48:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,18 @@
 class Dog 
-  def name=(dog_name)
-    @this_dog_name = dog_name
+  def initialize(dog_name)
+    @name = dog_name
   end
   
   def name 
-    @this_dog_name
+    @name
   end
   
-  def breed=(dog_breed)
-    @the_dog_breed = dog_breed
+  def initialize(dog_breed)
+    @breed = dog_breed
   end
   
   def breed
-    @the_dog_breed
+    @breed
   end
 end
 
